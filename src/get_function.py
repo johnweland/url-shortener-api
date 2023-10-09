c91218adacb8970510d78bcded232189faec3606
John Weland <john.weland@gmail.com>
2023-10-09 16:28:17 -0500

    begin setting up and testing GitHub Actions

--- src/get_function.py
+++ src/get_function.py
@@ -1,93 +1,83 @@
 """ GET Lambda. """
-from os import environ
 import json
-import boto3
 from http import HTTPStatus
+from os import environ
+
+import boto3
 from aws_lambda_powertools import Logger, Tracer
-from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
-from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.event_handler import (
     APIGatewayRestResolver,
     Response,
-    content_types
+    content_types,
 )
+from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
+from aws_lambda_powertools.utilities.typing import LambdaContext
 from botocore.exceptions import ClientError
 
-APP_NAME = environ.get("APP_NAME") or 'url-shortener GET'
-AWS_REGION = environ.get("AWS_REGION") or 'us-east-1'
-TABLE_NAME = environ.get("TABLE_NAME") or 'records'
-dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
+APP_NAME = environ.get("APP_NAME") or "url-shortener GET"
+AWS_REGION = environ.get("AWS_REGION") or "us-east-1"
+TABLE_NAME = environ.get("TABLE_NAME") or "records"
+dynamodb = boto3.resource("dynamodb", region_name=AWS_REGION)
 table = dynamodb.Table(TABLE_NAME)
 app = APIGatewayRestResolver()
 log: Logger = Logger(service=APP_NAME)
 trace: Tracer = Tracer(service=APP_NAME)
 
 
-@app.get('/api/v1/records')
+@app.get("/api/v1/records")
 @trace.capture_method
 def get_all_items() -> Response:
-    """ Get all items from DynamoDB table. """
+    """Get all items from DynamoDB table."""
     try:
         response = table.scan()
         return Response(
             status_code=HTTPStatus.OK.value,
             content_type=content_types.APPLICATION_JSON,
-            body=json.dumps({
-                "Count": response["Count"],
-                "Items": response["Items"],
-                "Scanned": response["ScannedCount"]
-            })
+            body=json.dumps(
+                {
+                    "Count": response["Count"],
+                    "Items": response["Items"],
+                    "Scanned": response["ScannedCount"],
+                }
+            ),
         )
     except ClientError as error:
-        log.error(error.response['Error']['Message'])
+        log.error(error.response["Error"]["Message"])
         return Response(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
-            body=json.dumps({
-                'message': error.response['Error']['Message']
-            })
+            body=json.dumps({"message": error.response["Error"]["Message"]}),
         )
 
 
-@app.get('/api/v1/records/<id>')
+@app.get("/api/v1/records/<id>")
 @trace.capture_method
 def get_item_by_id(id: str) -> Response:
-    """ Get a item from DynamoDB table. """
+    """Get a item from DynamoDB table."""
     try:
-        item = table.get_item(
-            Key={
-                'id': id
-            }
-        ).get('Item')
+        item = table.get_item(Key={"id": id}).get("Item")
 
         if not item:
             return Response(
                 status_code=HTTPStatus.NOT_FOUND.value,
-                body=json.dumps({
-                    'message': 'URL not found'
-                })
+                body=json.dumps({"message": "URL not found"}),
             )
         # return status code 302 and set redirect to url
         return Response(
             status_code=HTTPStatus.FOUND.value,
             content_type=content_types.APPLICATION_JSON,
-            headers={
-                "Location": str(item['url'])
-            }
+            headers={"Location": str(item["url"])},
         )
     except ClientError as error:
-        log.error(error.response['Error']['Message'])
+        log.error(error.response["Error"]["Message"])
         return Response(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
-            body=json.dumps({
-                'message': error.response['Error']['Message']
-            })
+            body=json.dumps({"message": error.response["Error"]["Message"]}),
         )
 
 
 @trace.capture_lambda_handler
 def lambda_handler(
-    event: APIGatewayProxyEvent,
-    context: LambdaContext
+    event: APIGatewayProxyEvent, context: LambdaContext
 ) -> dict[str, any]:
-    """ Lambda handler. """
+    """Lambda handler."""
     return app.resolve(event, context)
--- test/unit/test_get_function.py
+++ test/unit/test_get_function.py
@@ -1,46 +1,42 @@
 """ Unit Tests for GET Lambda. """
-import sys
-import os
 import json
+import os
+import sys
 from http import HTTPStatus
-import boto3
-from botocore.exceptions import ClientError
 from unittest import TestCase
 from unittest.mock import Mock, patch
+
+import boto3
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from botocore.exceptions import ClientError
 from moto import mock_dynamodb
+
 sys.path.append(os.path.abspath("."))
 
 
 @mock_dynamodb
 class test_get_function(TestCase):
-    """ Test GET Lambda. """
+    """Test GET Lambda."""
 
     def setUp(self):
-        """ Setup before each test. """
+        """Setup before each test."""
         super().setUp()
-        self.table_name = 'records'
-        self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+        self.table_name = "records"
+        self.dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
         self.dynamodb.create_table(
             TableName=self.table_name,
             KeySchema=[
-                {'AttributeName': 'id', 'KeyType': 'HASH'},
+                {"AttributeName": "id", "KeyType": "HASH"},
             ],
             AttributeDefinitions=[
-                {'AttributeName': 'id', 'AttributeType': 'S'},
+                {"AttributeName": "id", "AttributeType": "S"},
             ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 1,
-                'WriteCapacityUnits': 1
-            }
+            ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
         )
         self.table = self.dynamodb.Table(self.table_name)
-        from src.get_function import (
-            lambda_handler,
-            get_all_items,
-            get_item_by_id
-        )
+        from src.get_function import get_all_items, get_item_by_id, lambda_handler
+
         self.lambda_handler = lambda_handler
         self.get_all_items = get_all_items
         self.get_items_by_id = get_item_by_id
@@ -50,132 +46,111 @@ class test_get_function(TestCase):
         table = self.table
         table.put_item(
             Item={
-                'id': 'de305d54',
-                'url': 'https://www.google.com',
-                'createdAt': '2021-01-01T00:00:00.000Z'
+                "id": "de305d54",
+                "url": "https://www.google.com",
+                "createdAt": "2021-01-01T00:00:00.000Z",
             }
         )
         table.put_item(
             Item={
-                'id': '75b4431b',
-                'url': 'https://www.example.com',
-                'createdAt': '2022-03-08T00:00:00.000Z'
+                "id": "75b4431b",
+                "url": "https://www.example.com",
+                "createdAt": "2022-03-08T00:00:00.000Z",
             }
         )
 
     def test_get_all_items(self):
-        """ Test get_all_items function. """
+        """Test get_all_items function."""
         event = APIGatewayProxyEvent(
+            data={
+                "path": "/api/v1/records",
+                "httpMethod": "GET",
+                "headers": {"Content-Type": "application/json"},
+            }
+        )
+        context: LambdaContext = Mock()
+        response = self.lambda_handler(event, context)
+        self.assertEqual(response["statusCode"], HTTPStatus.OK.value)
+        self.assertEqual(json.loads(response["body"])["Count"], 2)
+        self.assertEqual(json.loads(response["body"])["Items"][0]["id"], "de305d54")
+        self.assertEqual(
+            json.loads(response["body"])["Items"][0]["url"], "https://www.google.com"
+        )
+        self.assertEqual(json.loads(response["body"])["Items"][1]["id"], "75b4431b")
+        self.assertEqual(
+            json.loads(response["body"])["Items"][1]["url"], "https://www.example.com"
+        )
+
+    def test_get_item_by_id(self):
+        """Test get_item_by_id function."""
+        event = APIGatewayProxyEvent(
+            data={
+                "path": "/api/v1/records/de305d54",
+                "httpMethod": "GET",
+                "headers": {"Content-Type": "application/json"},
+            }
+        )
+        context: LambdaContext = Mock()
+        response = self.lambda_handler(event, context)
+        str_response = json.dumps(response["multiValueHeaders"]["Location"][0])
+        self.assertEqual(response["statusCode"], HTTPStatus.FOUND.value)
+        self.assertEqual(json.loads(str_response), "https://www.google.com")
+
+    def test_get_item_by_id_not_found(self):
+        """Test get_item_by_id function."""
+        event = APIGatewayProxyEvent(
+            data={
+                "path": "/api/v1/records/123",
+                "httpMethod": "GET",
+                "headers": {"Content-Type": "application/json"},
+            }
+        )
+        context: LambdaContext = Mock()
+        response = self.lambda_handler(event, context)
+        self.assertEqual(response["statusCode"], HTTPStatus.NOT_FOUND.value)
+        self.assertEqual(json.loads(response["body"])["message"], "URL not found")
+
+    def test_get_all_items_error(self):
+        """Test get_all_items function when there is an error."""
+        with patch("src.get_function.table.scan") as mock_scan:
+            mock_scan.side_effect = ClientError(
+                {"Error": {"Code": "500", "Message": "Internal Server Error"}}, "scan"
+            )
+            event = APIGatewayProxyEvent(
                 data={
                     "path": "/api/v1/records",
                     "httpMethod": "GET",
                     "headers": {"Content-Type": "application/json"},
                 }
             )
-        context: LambdaContext = Mock()
-        response = self.lambda_handler(event, context)
-        self.assertEqual(response['statusCode'], HTTPStatus.OK.value)
-        self.assertEqual(
-            json.loads(response['body'])['Count'], 2
-        )
-        self.assertEqual(
-            json.loads(response['body'])['Items'][0]['id'],
-            'de305d54'
-        )
-        self.assertEqual(
-            json.loads(response['body'])['Items'][0]['url'],
-            'https://www.google.com'
-        )
-        self.assertEqual(
-            json.loads(response['body'])['Items'][1]['id'],
-            '75b4431b'
-        )
-        self.assertEqual(
-            json.loads(response['body'])['Items'][1]['url'],
-            'https://www.example.com'
-        )
+            context: LambdaContext = Mock()
+            response = self.lambda_handler(event, context)
+            self.assertEqual(
+                response["statusCode"], HTTPStatus.INTERNAL_SERVER_ERROR.value
+            )
+            self.assertEqual(
+                json.loads(response["body"])["message"], "Internal Server Error"
+            )
 
-    def test_get_item_by_id(self):
-        """ Test get_item_by_id function. """
-        event = APIGatewayProxyEvent(
+    def test_get_item_by_id_error(self):
+        """Test get_item_by_id function when there is an error."""
+        with patch("src.get_function.table.get_item") as mock_get_item:
+            mock_get_item.side_effect = ClientError(
+                {"Error": {"Code": "500", "Message": "Internal Server Error"}},
+                "get_item",
+            )
+            event = APIGatewayProxyEvent(
                 data={
                     "path": "/api/v1/records/de305d54",
                     "httpMethod": "GET",
                     "headers": {"Content-Type": "application/json"},
                 }
             )
-        context: LambdaContext = Mock()
-        response = self.lambda_handler(event, context)
-        str_response = json.dumps(response['multiValueHeaders']['Location'][0])
-        self.assertEqual(response['statusCode'], HTTPStatus.FOUND.value)
-        self.assertEqual(
-            json.loads(str_response),
-            'https://www.google.com'
-        )
-
-    def test_get_item_by_id_not_found(self):
-        """ Test get_item_by_id function. """
-        event = APIGatewayProxyEvent(
-                data={
-                    "path": "/api/v1/records/123",
-                    "httpMethod": "GET",
-                    "headers": {"Content-Type": "application/json"},
-                }
-            )
-        context: LambdaContext = Mock()
-        response = self.lambda_handler(event, context)
-        self.assertEqual(response['statusCode'], HTTPStatus.NOT_FOUND.value)
-        self.assertEqual(
-            json.loads(response['body'])['message'],
-            'URL not found'
-        )
-
-    def test_get_all_items_error(self):
-        """ Test get_all_items function when there is an error. """
-        with patch('src.get_function.table.scan') as mock_scan:
-            mock_scan.side_effect = ClientError(
-                {'Error': {'Code': '500', 'Message': 'Internal Server Error'}},
-                'scan'
-            )
-            event = APIGatewayProxyEvent(
-                    data={
-                        "path": "/api/v1/records",
-                        "httpMethod": "GET",
-                        "headers": {"Content-Type": "application/json"},
-                    }
-                )
             context: LambdaContext = Mock()
             response = self.lambda_handler(event, context)
             self.assertEqual(
-                response['statusCode'],
-                HTTPStatus.INTERNAL_SERVER_ERROR.value
+                response["statusCode"], HTTPStatus.INTERNAL_SERVER_ERROR.value
             )
             self.assertEqual(
-                json.loads(response['body'])['message'],
-                'Internal Server Error'
-            )
-
-    def test_get_item_by_id_error(self):
-        """ Test get_item_by_id function when there is an error. """
-        with patch('src.get_function.table.get_item') as mock_get_item:
-            mock_get_item.side_effect = ClientError(
-                {'Error': {'Code': '500', 'Message': 'Internal Server Error'}},
-                'get_item'
-            )
-            event = APIGatewayProxyEvent(
-                    data={
-                        "path": "/api/v1/records/de305d54",
-                        "httpMethod": "GET",
-                        "headers": {"Content-Type": "application/json"},
-                    }
-                )
-            context: LambdaContext = Mock()
-            response = self.lambda_handler(event, context)
-            self.assertEqual(
-                response['statusCode'],
-                HTTPStatus.INTERNAL_SERVER_ERROR.value
-            )
-            self.assertEqual(
-                json.loads(response['body'])['message'],
-                'Internal Server Error'
+                json.loads(response["body"])["message"], "Internal Server Error"
             )
